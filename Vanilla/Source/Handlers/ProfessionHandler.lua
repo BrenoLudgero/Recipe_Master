2cec76488d4c5fb4a4eb12f1eb490c37e397b841
BrenoLudgero <breno.ludgero@yahoo.com>
2024-04-07 21:32:05 -0300

    Split SpecializationHandler from ProfessionHandler

--- Vanilla/Source/Handlers/ProfessionHandler.lua
+++ Vanilla/Source/Handlers/ProfessionHandler.lua
@@ -38,33 +38,6 @@ local function getAllLearnedProfessions()
     return currentProfessions
 end
 
-local specializationIDs = {
-    [164] = {9788, 17041, 17040, 17039, 9787},
-    [202] = {20219, 20222},
-    [165] = {10657, 10658, 10660}
-}
-
-local function storeCurrentSpecializations(currentSpecializations, spellID)
-    for professionID, specs in pairs(specializationIDs) do
-        if rm.tableContains(specs, spellID) then
-            currentSpecializations[professionID] = spellID
-        end
-    end
-end
-
-local function getAllProfessionsSpecialization()
-    local currentSpecializations = {}
-    local numSpellTabs = GetNumSpellTabs()
-    for i = 1, numSpellTabs do
-        local offset, numSlots = select(3, GetSpellTabInfo(i))
-        for j = offset + 1, offset + numSlots do
-            local _, _, spellID = GetSpellBookItemName(j, BOOKTYPE_SPELL)
-            storeCurrentSpecializations(currentSpecializations, spellID)
-        end
-    end
-    return currentSpecializations
-end
-
 function rm.getSavedProfessionByID(professionID)
     return rm.getCharacterSavedVariables()[professionID]
 end
@@ -77,14 +50,6 @@ function getSavedProfessionRank(profession)
     return rm.getSavedProfessionByName(profession)["rank"]
 end
 
-function rm.getSavedSpecializationByID(professionID)
-    return rm.getCharacterSavedVariables()[professionID]["specialization"]
-end
-
-function rm.getSavedSpecializationByName(profession)
-    return rm.getCharacterSavedVariables()[rm.getProfessionID(profession)]["specialization"]
-end
-
 -- Stores the character's current professions in SavedVariables if not saved yet
 local function saveCharacterProfessions(currentProfessions)
     for professionID in pairs(currentProfessions) do
@@ -94,43 +59,24 @@ local function saveCharacterProfessions(currentProfessions)
     end
 end
 
-local function professionAbandoned(currentProfessions, professionID)
+local function isProfessionAbandoned(currentProfessions, professionID)
     return not currentProfessions[professionID] and rm.getSavedProfessionByID(professionID)
 end
 
 -- Removes abandoned professions from SavedVariables
-local function removeAbandonedProfessions(currentProfessions, professionID)
-    if professionAbandoned(currentProfessions, professionID) then
+local function removeAbandonedProfession(currentProfessions, professionID)
+    if isProfessionAbandoned(currentProfessions, professionID) then
         rm.getCharacterSavedVariables()[professionID] = nil
-    end
-end
-
--- Saves all current professions' specializations if any
-local function saveProfessionsSpecializations(currentSpecializations)
-    for professionID, specializationID in pairs(currentSpecializations) do
-        if not rm.getSavedSpecializationByID(professionID) then
-            rm.getSavedProfessionByID(professionID)["specialization"] = specializationID
-        end
-    end
-end
-
-local function specializationAbandoned(currentSpecializations, professionID)
-    return not currentSpecializations[professionID] and rm.getSavedSpecializationByID(professionID)
-end
-
-local function removeAbandonedSpecializations(currentSpecializations, professionID)
-    if specializationAbandoned(currentSpecializations, professionID) then
-        rm.getSavedProfessionByID(professionID)["specialization"] = nil
     end
 end
 
 function rm.updateCharacterProfessions()
     local currentProfessions = getAllLearnedProfessions()
-    local currentSpecializations = getAllProfessionsSpecialization()
+    local currentSpecializations = rm.getAllProfessionsSpecialization()
     saveCharacterProfessions(currentProfessions)
-    saveProfessionsSpecializations(currentSpecializations)
+    rm.saveProfessionsSpecializations(currentSpecializations)
     for professionID in pairs(rm.getCharacterSavedVariables()) do
-        removeAbandonedProfessions(currentProfessions, professionID)
-        removeAbandonedSpecializations(currentSpecializations, professionID)
+        removeAbandonedProfession(currentProfessions, professionID)
+        rm.removeAbandonedSpecialization(currentSpecializations, professionID)
     end
 end
---
+++ Vanilla/Source/Handlers/SpecializationHandler.lua
@@ -0,0 +1,55 @@
+local _, rm = ...
+
+local specializationIDs = {
+    [164] = {9788, 17041, 17040, 17039, 9787},
+    [202] = {20219, 20222},
+    [165] = {10657, 10658, 10660}
+}
+
+local function storeCurrentSpecializations(currentSpecializations, spellID)
+    for professionID, specs in pairs(specializationIDs) do
+        if rm.tableContains(specs, spellID) then
+            currentSpecializations[professionID] = spellID
+        end
+    end
+end
+
+function rm.getAllProfessionsSpecialization()
+    local currentSpecializations = {}
+    local numSpellTabs = GetNumSpellTabs()
+    for i = 1, numSpellTabs do
+        local offset, numSlots = select(3, GetSpellTabInfo(i))
+        for j = offset + 1, offset + numSlots do
+            local _, _, spellID = GetSpellBookItemName(j, BOOKTYPE_SPELL)
+            storeCurrentSpecializations(currentSpecializations, spellID)
+        end
+    end
+    return currentSpecializations
+end
+
+function rm.getSavedSpecializationByID(professionID)
+    return rm.getCharacterSavedVariables()[professionID]["specialization"]
+end
+
+function rm.getSavedSpecializationByName(profession)
+    return rm.getCharacterSavedVariables()[rm.getProfessionID(profession)]["specialization"]
+end
+
+-- Saves all current professions' specializations if any
+function rm.saveProfessionsSpecializations(currentSpecializations)
+    for professionID, specializationID in pairs(currentSpecializations) do
+        if not rm.getSavedSpecializationByID(professionID) then
+            rm.getSavedProfessionByID(professionID)["specialization"] = specializationID
+        end
+    end
+end
+
+local function isSpecializationAbandoned(currentSpecializations, professionID)
+    return not currentSpecializations[professionID] and rm.getSavedSpecializationByID(professionID)
+end
+
+function rm.removeAbandonedSpecialization(currentSpecializations, professionID)
+    if isSpecializationAbandoned(currentSpecializations, professionID) then
+        rm.getSavedProfessionByID(professionID)["specialization"] = nil
+    end
+end
