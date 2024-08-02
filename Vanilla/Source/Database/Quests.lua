13f3a3ee1d8e4bf11b01eecf90f6f8d2b0e28c57
BrenoLudgero <breno.ludgero@yahoo.com>
2024-08-02 16:24:56 -0300

    Scrape sources' NPCs, objects, quests

---
+++ Vanilla/Source/Database/Quests.lua
@@ -0,0 +1,427 @@
+local _, rm = ...
+
+rm.questDB = {
+	[769] = {
+		faction = "Horde",
+		level = 10,
+		reqLevel = 4,
+	},
+	[5124] = {
+		classification = "Elite",
+		level = 60,
+		reqLevel = 55,
+	},
+	[5127] = {
+		classification = "Dungeon",
+		level = 60,
+		reqLevel = 55,
+	},
+	[84495] = {
+		classification = "Elite",
+		level = 60,
+		reqLevel = 55,
+	},
+	[84496] = {
+		classification = "Dungeon",
+		level = 60,
+		reqLevel = 50,
+	},
+	[2853] = {
+		faction = "Alliance",
+		level = 45,
+		reqLevel = 30,
+	},
+	[22] = {
+		faction = "Alliance",
+		level = 12,
+		reqLevel = 9,
+	},
+	[1559] = {
+		level = 37,
+		reqLevel = 30,
+	},
+	[2848] = {
+		faction = "Alliance",
+		level = 45,
+		reqLevel = 30,
+	},
+	[9249] = {
+		level = 60,
+		reqLevel = 45,
+	},
+	[2849] = {
+		faction = "Alliance",
+		level = 45,
+		reqLevel = 30,
+	},
+	[2850] = {
+		faction = "Alliance",
+		level = 45,
+		reqLevel = 30,
+	},
+	[2851] = {
+		faction = "Alliance",
+		level = 45,
+		reqLevel = 30,
+	},
+	[2852] = {
+		faction = "Alliance",
+		level = 45,
+		reqLevel = 30,
+	},
+	[38] = {
+		faction = "Alliance",
+		level = 13,
+		reqLevel = 9,
+	},
+	[2855] = {
+		faction = "Horde",
+		level = 45,
+		reqLevel = 30,
+	},
+	[296] = {
+		faction = "Alliance",
+		level = 29,
+		reqLevel = 22,
+	},
+	[80169] = {
+		level = 60,
+		reqLevel = 1,
+	},
+	[1578] = {
+		faction = "Alliance",
+		level = 12,
+		reqLevel = 1,
+	},
+	[555] = {
+		faction = "Alliance",
+		level = 31,
+		reqLevel = 28,
+	},
+	[80168] = {
+		level = 60,
+		reqLevel = 1,
+	},
+	[2856] = {
+		faction = "Horde",
+		level = 45,
+		reqLevel = 30,
+	},
+	[1582] = {
+		faction = "Alliance",
+		level = 18,
+		reqLevel = 8,
+	},
+	[2857] = {
+		faction = "Horde",
+		level = 45,
+		reqLevel = 30,
+	},
+	[2858] = {
+		faction = "Horde",
+		level = 45,
+		reqLevel = 30,
+	},
+	[2859] = {
+		faction = "Horde",
+		level = 45,
+		reqLevel = 30,
+	},
+	[2860] = {
+		faction = "Horde",
+		level = 45,
+		reqLevel = 30,
+	},
+	[80170] = {
+		level = 60,
+		reqLevel = 1,
+	},
+	[564] = {
+		faction = "Alliance",
+		level = 34,
+		reqLevel = 30,
+	},
+	[2359] = {
+		faction = "Alliance",
+		classification = "Elite",
+		level = 24,
+		reqLevel = 20,
+	},
+	[4161] = {
+		faction = "Alliance",
+		level = 7,
+		reqLevel = 1,
+	},
+	[7493] = {
+		faction = "Horde",
+		level = 60,
+		reqLevel = 60,
+	},
+	[7497] = {
+		faction = "Alliance",
+		level = 60,
+		reqLevel = 60,
+	},
+	[3402] = {
+		level = 50,
+		reqLevel = 40,
+	},
+	[1618] = {
+		faction = "Alliance",
+		level = 16,
+		reqLevel = 1,
+	},
+	[90] = {
+		faction = "Alliance",
+		level = 25,
+		reqLevel = 18,
+	},
+	[92] = {
+		faction = "Alliance",
+		level = 18,
+		reqLevel = 15,
+	},
+	[93] = {
+		faction = "Alliance",
+		level = 20,
+		reqLevel = 17,
+	},
+	[862] = {
+		faction = "Horde",
+		level = 23,
+		reqLevel = 15,
+	},
+	[80241] = {
+		faction = "Alliance",
+		level = 40,
+		reqLevel = 40,
+	},
+	[84338] = {
+		classification = "Dungeon",
+		level = 60,
+		reqLevel = 60,
+	},
+	[82657] = {
+		faction = "Alliance",
+		level = 45,
+		reqLevel = 30,
+	},
+	[127] = {
+		faction = "Alliance",
+		level = 21,
+		reqLevel = 16,
+	},
+	[384] = {
+		faction = "Alliance",
+		level = 7,
+		reqLevel = 5,
+	},
+	[385] = {
+		faction = "Alliance",
+		level = 15,
+		reqLevel = 10,
+	},
+	[2178] = {
+		faction = "Alliance",
+		level = 12,
+		reqLevel = 9,
+	},
+	[8586] = {
+		level = 60,
+		reqLevel = 60,
+	},
+	[7321] = {
+		faction = "Horde",
+		level = 31,
+		reqLevel = 28,
+	},
+	[418] = {
+		faction = "Alliance",
+		level = 11,
+		reqLevel = 7,
+	},
+	[8869] = {
+		classification = "Dungeon",
+		level = 60,
+		reqLevel = 50,
+	},
+	[429] = {
+		faction = "Horde",
+		level = 11,
+		reqLevel = 10,
+	},
+	[2478] = {
+		faction = "Horde",
+		classification = "Elite",
+		level = 24,
+		reqLevel = 20,
+	},
+	[8877] = {
+		level = 60,
+		reqLevel = 45,
+	},
+	[8878] = {
+		level = 60,
+		reqLevel = 50,
+	},
+	[8882] = {
+		level = 60,
+		reqLevel = 55,
+	},
+	[7604] = {
+		classification = "Dungeon",
+		level = 60,
+		reqLevel = 60,
+	},
+	[5305] = {
+		classification = "Dungeon",
+		level = 60,
+		reqLevel = 50,
+	},
+	[5306] = {
+		classification = "Dungeon",
+		level = 60,
+		reqLevel = 50,
+	},
+	[5307] = {
+		classification = "Dungeon",
+		level = 60,
+		reqLevel = 50,
+	},
+	[2751] = {
+		faction = "Horde",
+		level = 32,
+		reqLevel = 32,
+	},
+	[2752] = {
+		faction = "Horde",
+		level = 32,
+		reqLevel = 32,
+	},
+	[2753] = {
+		faction = "Horde",
+		level = 36,
+		reqLevel = 32,
+	},
+	[2754] = {
+		faction = "Horde",
+		level = 36,
+		reqLevel = 32,
+	},
+	[2755] = {
+		faction = "Horde",
+		level = 36,
+		reqLevel = 32,
+	},
+	[703] = {
+		level = 40,
+		reqLevel = 33,
+	},
+	[2758] = {
+		faction = "Alliance",
+		level = 40,
+		reqLevel = 40,
+	},
+	[2761] = {
+		level = 45,
+		reqLevel = 40,
+	},
+	[2762] = {
+		level = 45,
+		reqLevel = 40,
+	},
+	[715] = {
+		level = 37,
+		reqLevel = 35,
+	},
+	[2763] = {
+		level = 45,
+		reqLevel = 40,
+	},
+	[6607] = {
+		level = 45,
+		reqLevel = 35,
+	},
+	[1487] = {
+		classification = "Dungeon",
+		level = 21,
+		reqLevel = 15,
+	},
+	[6610] = {
+		level = 45,
+		reqLevel = 35,
+	},
+	[471] = {
+		faction = "Alliance",
+		level = 26,
+		reqLevel = 18,
+	},
+	[6622] = {
+		faction = "Horde",
+		level = 45,
+		reqLevel = 35,
+	},
+	[6624] = {
+		faction = "Alliance",
+		level = 45,
+		reqLevel = 35,
+	},
+	[7649] = {
+		classification = "Dungeon",
+		level = 60,
+		reqLevel = 50,
+	},
+	[7650] = {
+		classification = "Dungeon",
+		level = 60,
+		reqLevel = 50,
+	},
+	[7651] = {
+		classification = "Dungeon",
+		level = 60,
+		reqLevel = 50,
+	},
+	[82656] = {
+		faction = "Horde",
+		level = 45,
+		reqLevel = 30,
+	},
+	[7653] = {
+		level = 60,
+		reqLevel = 50,
+	},
+	[7654] = {
+		level = 60,
+		reqLevel = 50,
+	},
+	[7655] = {
+		level = 60,
+		reqLevel = 50,
+	},
+	[7656] = {
+		level = 60,
+		reqLevel = 50,
+	},
+	[7657] = {
+		level = 60,
+		reqLevel = 50,
+	},
+	[7658] = {
+		level = 60,
+		reqLevel = 50,
+	},
+	[7659] = {
+		level = 60,
+		reqLevel = 50,
+	},
+	[498] = {
+		faction = "Horde",
+		level = 22,
+		reqLevel = 17,
+	},
+	[501] = {
+		faction = "Horde",
+		level = 24,
+		reqLevel = 21,
+	},
+}
---
+++ Vanilla/Source/Database/Unique.lua
@@ -0,0 +1,93 @@
+local _, rm = ...
+
+rm.uniqueDB = {
+	[14401] = {
+		names = {
+			deDE = "Meisterelementarformer Krixix",
+			enUS = "Master Elemental Shaper Krixix",
+			esES = "Maestro de los elementos Formacio Krixix",
+			esMX = "Maestro de los elementos Formacio Krixix",
+			frFR = "Maître élémentaire Krixix le Sculpteur",
+			koKR = "수석 원소구현술사 크릭시스",
+			ptBR = "Mestre Moldador Elemental Krixix",
+			ruRU = "Ваятель стихий Криксикс",
+			zhCN = "大元素师克里希克",
+			zhTW = "大元素師剋裏希剋",
+		},
+		classification = "Elite",
+		level = "60",
+		zones = {
+			2677,
+		},
+	},
+	[10503] = {
+		names = {
+			enUS = "Jandice Barov",
+			koKR = "잔다이스 바로브",
+			ptBR = "Janice Barov",
+			ruRU = "Джандис Баров",
+			zhCN = "詹迪斯",
+			zhTW = "詹迪斯",
+		},
+		classification = "Elite",
+		level = "61",
+		zones = {
+			2057,
+		},
+	},
+	[9037] = {
+		names = {
+			deDE = "Dunk'rel",
+			enUS = "Gloom'rel",
+			frFR = "Funéb'rel",
+			koKR = "그늘의 문지기",
+			ptBR = "Umbra'rel",
+			ruRU = "Мрак'нел",
+			zhCN = "格鲁雷尔",
+			zhTW = "格魯雷爾",
+		},
+		classification = "Elite",
+		level = "56",
+		zones = {
+			1584,
+		},
+	},
+	[16365] = {
+		names = {
+			deDE = "Meisterhandwerker Omarion",
+			enUS = "Master Craftsman Omarion",
+			esES = "Maestro artesano Omarion",
+			esMX = "Maestro artesano Omarion",
+			frFR = "Maître-artisan Omarion",
+			koKR = "거장 오마리온",
+			ptBR = "Mestre Artesão Omarion",
+			ruRU = "Мастер-ремесленник Омарион",
+			zhCN = "大工匠奥玛里恩",
+			zhTW = "大工匠奧瑪裏恩",
+		},
+		classification = "Elite",
+		level = "60",
+		zones = {
+			3456,
+		},
+	},
+	[8983] = {
+		names = {
+			deDE = "Golemlord Argelmach",
+			enUS = "Golem Lord Argelmach",
+			esES = "Señor Gólem Argelmach",
+			esMX = "Señor Gólem Argelmach",
+			frFR = "Seigneur golem Argelmach",
+			koKR = "골렘 군주 아젤마크",
+			ptBR = "Lorde Golem Argelmach",
+			ruRU = "Повелитель големов Аргелмах",
+			zhCN = "傀儡统帅阿格曼奇",
+			zhTW = "傀儡統帥阿格曼奇",
+		},
+		classification = "Elite",
+		level = "57",
+		zones = {
+			1584,
+		},
+	},
+}
