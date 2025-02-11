7136802d4954ed29901c2c49e309985536c54c98
BrenoLudgero <breno.ludgero@yahoo.com>
2025-02-11 11:25:29 -0300

    Update Cataclysm Spanish localization

--- Cataclysm/Source/Localization.lua
+++ Cataclysm/Source/Localization.lua
@@ -372,7 +372,7 @@ elseif rm.locale == "esMX" or rm.locale == "esES" then
     L.blue = "Azul"
     L.bright = "Brillante"
     L.canLearn = "Puede Aprender"
-    L.chance = "Chance"
+    L.chance = "Prob."
     L.dark = "Oscura"
     L.difficult = "Difícil"
     L.difficulty = "Dificultad"
@@ -394,7 +394,7 @@ elseif rm.locale == "esMX" or rm.locale == "esES" then
     L.showSourcesTooltipInfo = "Mostrar fuentes"
     L.stock = "Reserva"
     L.trivial = "Trivial"
-    L.updateIconDropdown = "Ícono de Restauración de la Ventana"
+    L.updateIconDropdown = "Icono de Restauración de la Ventana"
     L.pickpocket = "Robo"
     L.trainer = "Instructor"
     return
