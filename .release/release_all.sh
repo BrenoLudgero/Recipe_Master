515533daf975ee3d935983c707a8d75a9887d626
BrenoLudgero <breno.ludgero@yahoo.com>
2025-03-16 21:58:59 -0300

    Update packaging process

--- .release/release_all.sh
+++ .release/release_all.sh
@@ -1,2 +1,22 @@
+#!/bin/bash
+
+git fetch --tags
+
+CURRENT_TAG=$(git describe --tags --abbrev=0)
+
+read -p "Enter new version tag (current: $CURRENT_TAG): " NEW_TAG
+
+# Deletes the current tag locally and remotely if NEW_TAG matches CURRENT_TAG
+if [ "$NEW_TAG" == "$CURRENT_TAG" ]; then
+    git tag --delete "$CURRENT_TAG"
+    git push origin --delete "$CURRENT_TAG"
+fi
+
+git tag "$NEW_TAG"
+git push origin "$NEW_TAG"
+git checkout "$NEW_TAG"
+
 sudo sh release.sh -m vanilla.yaml
 sudo sh release.sh -m cataclysm.yaml
+
+git checkout "main"
