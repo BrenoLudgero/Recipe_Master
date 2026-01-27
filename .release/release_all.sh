c3a6ee024fc1bc0503a40a9815c19925ff230578
BrenoLudgero <breno.ludgero@yahoo.com>
2026-01-26 22:18:44 -0300

    Updated packager

--- .release/release_all.sh
+++ .release/release_all.sh
@@ -2,14 +2,14 @@
 
 git fetch --tags
 
-CURRENT_TAG=$(git describe --tags --abbrev=0)
+LATEST_REMOTE_TAG=$(git describe --tags --abbrev=0)
 
-read -p "Enter new version tag (current: $CURRENT_TAG): " NEW_TAG
+read -p "Enter new version tag (current: $LATEST_REMOTE_TAG): " NEW_TAG
 
 # Deletes the current tag locally and remotely if NEW_TAG matches CURRENT_TAG
-if [ "$NEW_TAG" == "$CURRENT_TAG" ]; then
-    git tag --delete "$CURRENT_TAG"
-    git push origin --delete "$CURRENT_TAG"
+if [ "$NEW_TAG" == "$LATEST_REMOTE_TAG" ]; then
+    git tag --delete "$LATEST_REMOTE_TAG"
+    gh release delete "$LATEST_REMOTE_TAG" -y
 fi
 
 git tag "$NEW_TAG"
@@ -17,6 +17,5 @@ git push origin "$NEW_TAG"
 git checkout "$NEW_TAG"
 
 sudo sh release.sh -m vanilla.yaml
-# sudo sh release.sh -m cataclysm.yaml
 
 git checkout "main"
