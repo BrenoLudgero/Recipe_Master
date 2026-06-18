79dfe300da023614b8663c40b536d6d698b79210
BrenoLudgero <breno.ludgero@yahoo.com>
2026-06-18 11:50:58 -0300

    Updated packager and improved its initiation script

--- .release/release_all.sh
+++ .release/release_all.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+set -e
+
 git fetch --tags
 
 LATEST_REMOTE_TAG=$(git describe --tags --abbrev=0)
 
 read -p "Enter new version tag (current: $LATEST_REMOTE_TAG): " NEW_TAG
 
-# Deletes the current tag locally and remotely if NEW_TAG matches CURRENT_TAG
-if [ "$NEW_TAG" == "$LATEST_REMOTE_TAG" ]; then
+if [ -z "$NEW_TAG" ]; then
+    echo "No tag entered, aborting."
+    exit 1
+fi
+
+# If NEW_TAG matches LATEST_REMOTE_TAG, deletes the
+# current tag (locally and remotely) and latest release
+if [ "$NEW_TAG" = "$LATEST_REMOTE_TAG" ]; then
     git tag --delete "$LATEST_REMOTE_TAG"
     gh release delete "$LATEST_REMOTE_TAG" -y
+    git push remote --delete "$LATEST_REMOTE_TAG"
 fi
 
 git tag "$NEW_TAG"
